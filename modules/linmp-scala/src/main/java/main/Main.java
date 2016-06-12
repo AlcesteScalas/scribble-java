c36e2d4eb9d92574140290fe29061d8da9479d77
Ray Hu <raymond.hu05@imperial.ac.uk>
2016-06-12 20:43:11 +0100

    Minor.

--- modules/linmp-scala/src/main/java/main/Main.java
+++ modules/linmp-scala/src/main/java/main/Main.java
@@ -22,36 +22,38 @@ public class Main
 		String proto = "Proto";  // Hardcoded to look for protocol named "Proto"
 		
 		ScribProtocolTranslator sbp = new ScribProtocolTranslator();
+		GlobalType g = null;
 		try
 		{
-			GlobalType g = sbp.parseAndCheck(mainmod, proto);
+			g = sbp.parseAndCheck(mainmod, proto);
 			System.out.println("Translated:\n" + "    " + g);
-			
-			GlobalType gs = ast.global.ops.Sanitizer.apply(g);
-			System.out.println("\nSanitized:\n" + "    " + gs);
-			
-			Map<Role, LocalType> projs = ast.global.ops.Projector.apply(gs, ast.local.ops.Merge::full);
-			for (Entry<Role, LocalType> rl: projs.entrySet())
-			{
-				LocalType l = rl.getValue();
-				System.out.println("\nLocal projection for " + rl.getKey() + ":\n    " + l);
-	//			Map<Role, Type> p = ast.local.ops.Projector.apply(l, ast.binary.ops.Merge::full);
-	//			for (Role r: l.roles())
-	//			{
-	//				Type b = p.get(r);
-	//				System.out.println("Binary type towards " + r + ":\n    " + b);
-	//				ast.linear.Type bl = ast.binary.ops.LinearEncoder.apply(b);
-	//				System.out.println("    Linear encoding:\n        " + bl);
-	//				String scalaProt = ast.linear.ops.ScalaProtocolExtractor.apply(bl);
-	//				System.out.println("    Scala protocol classes:\n" + scalaProt);
-	//			}
-				String scalaMPProt = ast.local.ops.ScalaProtocolExtractor.apply(l);
-				System.out.println("    Scala protocol classes for local type:\n" + scalaMPProt);
-			}
 		}
 		catch (ScribParserException | ScribbleException e)
 		{
 			System.err.println(e.getMessage());
+			System.exit(1);
+		}
+		
+		GlobalType gs = ast.global.ops.Sanitizer.apply(g);
+		System.out.println("\nSanitized:\n" + "    " + gs);
+		
+		Map<Role, LocalType> projs = ast.global.ops.Projector.apply(gs, ast.local.ops.Merge::full);
+		for (Entry<Role, LocalType> rl: projs.entrySet())
+		{
+			LocalType l = rl.getValue();
+			System.out.println("\nLocal projection for " + rl.getKey() + ":\n    " + l);
+//			Map<Role, Type> p = ast.local.ops.Projector.apply(l, ast.binary.ops.Merge::full);
+//			for (Role r: l.roles())
+//			{
+//				Type b = p.get(r);
+//				System.out.println("Binary type towards " + r + ":\n    " + b);
+//				ast.linear.Type bl = ast.binary.ops.LinearEncoder.apply(b);
+//				System.out.println("    Linear encoding:\n        " + bl);
+//				String scalaProt = ast.linear.ops.ScalaProtocolExtractor.apply(bl);
+//				System.out.println("    Scala protocol classes:\n" + scalaProt);
+//			}
+			String scalaMPProt = ast.local.ops.ScalaProtocolExtractor.apply(l);
+			System.out.println("    Scala protocol classes for local type:\n" + scalaMPProt);
 		}
 	}
 }
