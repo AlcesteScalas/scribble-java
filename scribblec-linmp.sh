5fa1dc10672188076249c441288f62ebcf1b6e71
rhu1 <r.z.h.hu@herts.ac.uk>
2020-10-19 14:37:15 +0100

    Edit scribblec-linmp.sh

--- scribblec-linmp.sh
+++ scribblec-linmp.sh
@@ -4,11 +4,12 @@ CLASSPATH='modules/cli/target/classes/'
 CLASSPATH=$CLASSPATH':modules/core/target/classes/'
 CLASSPATH=$CLASSPATH':modules/parser/target/classes/'
 CLASSPATH=$CLASSPATH':modules/linmp-scala/target/classes/'
-CLASSPATH=$CLASSPATH':lib/antlr-runtime.jar'
-CLASSPATH=$CLASSPATH':lib/scribble-cli.jar'
-CLASSPATH=$CLASSPATH':lib/scribble-core.jar'
-CLASSPATH=$CLASSPATH':lib/scribble-parser.jar'
-CLASSPATH=$CLASSPATH':lib/linmp-scala.jar'
+CLASSPATH=$CLASSPATH':modules/parser/lib/antlr-3.5.2-complete.jar'
+#CLASSPATH=$CLASSPATH':lib/antlr-runtime.jar'
+#CLASSPATH=$CLASSPATH':lib/scribble-cli.jar'
+#CLASSPATH=$CLASSPATH':lib/scribble-core.jar'
+#CLASSPATH=$CLASSPATH':lib/scribble-parser.jar'
+#CLASSPATH=$CLASSPATH':lib/linmp-scala.jar'
 
 java -cp $CLASSPATH main.Main $1 $2
 
